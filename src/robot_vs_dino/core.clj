760100dd5b746c5eec9bd248879b5bf72d6cc221
Fabricio Pirini <fabricio.pirini@gmail.com>
2019-05-06 15:44:57 +0200

    Functions to add and print + tests for them

--- src/robot_vs_dino/core.clj
+++ src/robot_vs_dino/core.clj
@@ -1,57 +1,87 @@
 (ns robot-vs-dino.core
   (:gen-class))
 
+(def board-dimension 5)
+(def board-total-size 25)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
 
 (defn board
-  "Generates {size}x{size} board"
-  [size] (vec (repeat (* size size) "O")))
+  "Generates 50x50 board"
+  [] (vec (repeat board-total-size "⛶")))
+
+(defn print-board
+  "Print the actual board"
+  [board]
+  (if (nil? board)
+    "Invalid board"
+    (doseq [i (range board-total-size)]
+     (if (= (rem i board-dimension) (- board-dimension 1))
+      (println (get board i))
+      (print (str (get board i) " "))))))
+
+(defn get-board-pos
+  "Returns the element position on board due to its collumn and row numbers"
+  [col row]
+  (int (+ col(* (dec row) board-dimension))))
+
+(defn get-vector-pos
+  "Returns the element position on vector due to its collumn and row numbers"
+  [col row]
+  (dec (get-board-pos col row)))
 
 (defn inside-board?
   "Returns if the position is inside the board"
-  [pos board]
-  (if (or (>= pos (count board)) (neg? pos))
-   false
+  [pos]
+  (when (and (pos? pos) (< pos board-total-size))
    true))
-
-(defn get-board-size
-  "Returns the board size"
-  [board]
-  (Math/sqrt (count board)))
-
-(defn get-pos
-  "Returns the element position on vector due to its collumn and row numbers
-  being X for collumn and Y for row."
-  [X Y board]
-  (int (+ X(* (dec Y) (get-board-size board)))))
-
-(defn row-num
-  "Returns which row the position belongs to: pos 1 in row 1,
-  pos 51 in row 2, etc"
-  [pos board]
-  (when (inside-board? pos board)
-   (int (inc (quot (dec pos) (get-board-size board))))))
-
-(defn col-num
-  "Returns which collumn the position belongs to: pos 1 in row 1,
-  pos 50 in col 50, pos 51 in col 1 again, etc"
-  [pos board]
-  (when (inside-board? pos board)
-   (int (inc (rem (dec pos) (get-board-size board))))))
 
 (defn is-space-available?
   "Returns if the position on the board is free"
   [pos board]
-  (when (and (inside-board? pos board) (= '("O") (get board pos)))
+  ; (do (println "Is the pos 'O'?" (= "O" (get board (dec pos)))))
+  ; (do (println "Is it inside?" (inside-board? pos board)))
+  (when (and (inside-board? pos) (= "⛶" (get board (dec pos))))
    true))
+
+(defn row-num
+  "Returns which row the position belongs to: pos 1 in row 1,
+  pos 51 in row 2, etc"
+  [pos]
+  (when (inside-board? pos)
+   (int (inc (quot (dec pos) board-dimension)))))
+
+(defn col-num
+  "Returns which collumn the position belongs to: pos 1 in row 1,
+  pos 50 in col 50, pos 51 in col 1 again, etc"
+  [pos]
+  (when (inside-board? pos)
+   (int (inc (rem (dec pos) board-dimension)))))
+
+(defn get-symbol
+  "Returns the element symbol to be placed on board"
+  [letter]
+  (def letter-map {:F "🄵", "F" "🄵",
+                   :B "🄱", "B" "🄱",
+                   :L "🄻", "L" "🄻",
+                   :R "🅁", "R" "🅁"})
+  (get letter-map letter))
 
 (defn add-robot
   "Returns board with new element positioned on X collumn and Y row,
-  if successful; return nil, if not."
-  ([X Y board] (add-robot X Y "U" board))
-  ([X Y facing board]
-   (when is-space-available?
-    (assoc-in board [(dec (get-pos X Y board))] facing))))
+  if successful; return nil, if not"
+  ([col row board] (add-robot col row "F" board))
+  ([col row facing board]
+   (when (and (is-space-available? (get-board-pos col row) board)
+              (not (nil? (get-symbol facing))))
+     (assoc-in board [(get-vector-pos col row)] (get-symbol facing)))))
+
+(defn add-dino
+  "Returns board with new element positioned on X collumn and Y row,
+  if successful; return nil, if not"
+  [col row board]
+  (when (is-space-available? (get-board-pos col row) board)
+    (assoc-in board [(get-vector-pos col row)] "🄳")))
--- test/robot_vs_dino/core_test.clj
+++ test/robot_vs_dino/core_test.clj
@@ -2,32 +2,45 @@
   (:require [clojure.test :refer [deftest testing is]]
             [robot-vs-dino.core :as dino]))
 
-(def size 5)
-(def board (dino/board size))
+(def board-dimension 5)
+(def board (dino/board))
+
+(deftest get-pos-test
+  (testing "Get element in a defined position"
+    (is (= 1 (dino/get-board-pos 1 1)))
+    (is (= (int (+ (- board-dimension 3)(* (- board-dimension 3) board-dimension)))
+           (dino/get-board-pos (- board-dimension 3) (- board-dimension 2))))))
 
 (deftest row-num-test
   (testing "Check the row number of a position"
-    (is (= 1 (dino/row-num 1 board)))
-    (is (= 1 (dino/row-num size board)))
-    (is (= 2 (dino/row-num (inc size) board)))
-    (is (= 4 (dino/row-num (dec (* 4 size)) board)))))
+    (is (= 1 (dino/row-num 1)))
+    (is (= 1 (dino/row-num board-dimension)))
+    (is (= 2 (dino/row-num (inc board-dimension))))
+    (is (= 4 (dino/row-num (dec (* 4 board-dimension)))))))
 
 (deftest col-num-test
   (testing "Check the collumn number of a position"
-    (is (= 1 (dino/col-num 1 board)))
-    (is (= 1 (dino/col-num (inc size) board)))
-    (is (= size (dino/col-num size board)))
-    (is (= 1 (dino/col-num (inc size) board)))
-    (is (= (dec size) (dino/col-num (dec (* 4 size)) board)))))
+    (is (= 1 (dino/col-num 1)))
+    (is (= 1 (dino/col-num (inc board-dimension))))
+    (is (= board-dimension (dino/col-num board-dimension)))
+    (is (= 1 (dino/col-num (inc board-dimension))))
+    (is (= (dec board-dimension) (dino/col-num (dec (* 4 board-dimension)))))))
 
-(deftest check-board
+(deftest board-test
   (testing "Check board dimensions and elements on initialization"
-    (is (= (* size size) (count (dino/board size))))
-    (is (= '#{"O"} (set (dino/board size))))))
+    (is (= (* board-dimension board-dimension) (count (dino/board))))
+    (is (= '#{"⛶"} (set (dino/board))))))
 
-(deftest add-robot
+(deftest add-robot-test
   (testing "Add a robot to the simulation"
-    (is (= "R" (get (dino/add-robot 2 3 "R" board) (+ (dec 2)(* (dec 3) size)))))
-    (is (= "D" (get (dino/add-robot 3 2 "D" board) (+ (dec 3)(* (dec 2) size)))))
-    (is (= "U" (get (dino/add-robot 4 4 board) (+ (dec 4)(* (dec 4) size)))))
+    (is (= "🅁" (get (dino/add-robot 2 3 "R" board) (dino/get-vector-pos 2 3))))
+    (is (= "🄱" (get (dino/add-robot 3 2 "B" board) (dino/get-vector-pos 3 2))))
+    (is (= "🄵" (get (dino/add-robot 4 4 board) (dino/get-vector-pos 4 4))))
     (is (nil? (dino/add-robot 1 2 (dino/add-robot 1 2 board))))))
+
+(deftest add-dino-test
+  (testing "Add a dino to the simulation"
+    (is (= "🄳" (get (dino/add-dino 2 3 board) (dino/get-vector-pos 2 3))))
+    (is (= "🄳" (get (dino/add-dino 3 2 board) (dino/get-vector-pos 3 2))))
+    (is (= "⛶" (get (dino/add-dino 4 4 board) (dino/get-vector-pos 4 5))))
+    (is (nil? (dino/add-dino 1 2 (dino/add-dino 1 2 board))))))
